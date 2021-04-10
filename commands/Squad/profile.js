d1becaabfeafe0e4b2255ae6a1d692189770db46
Ethan Snow <bombitmanbomb@gmail.com>
2021-04-10 17:45:34 -0400

    MYSQLPromiseObjectBuilder

--- commands/Squad/profile.js
+++ commands/Squad/profile.js
@@ -2,6 +2,51 @@ const Command = require("../../base/Command.js"),
 	Discord = require("discord.js");
 
 const mysql = require("mysql");
+/**
+ * MySQL Response Object Builder.
+ * @author bombitmanbomb
+ * @class MYSQLPromiseObjectBuilder
+ */
+class MYSQLPromiseObjectBuilder {
+	constructor(/**@type {import("mysql").Pool}*/pool) {
+		this.keys = [];
+		this.values = [];
+		this.pool = pool;
+	}
+	/**Add a promise to the handler.
+	 * @param {string} key Key in the response object
+	 * @param {string} query SQL Query
+	 * @memberof MYSQLPromiseObjectBuilder
+	 * @returns {true} Done
+	 */
+	async add(key, query, def = null, DBKey) {
+		this.keys.push(key);
+		let response = new Promise((res, rej) => {
+			this.pool.query(query, (err, result) => { // Call from Pool, Auto closes connection
+				if (result && result[0] != null) {
+					res(DBKey != null ? result[0][DBKey] : result[0]);
+				} else {
+					res(def);
+				}
+			});
+		});
+		this.values.push(response);
+		return response;
+	}
+	/**Build an object based on the responses.
+	 * @returns {Object<*>} Object
+	 * @memberof MYSQLPromiseObjectBuilder
+	 */
+	async waitForAll() {
+		let values = await Promise.all(this.values);
+		let response = {};
+		for (let i = 0; i < values.length; i++) {
+			response[this.keys[i]] = values[i];
+		}
+		return response;
+	}
+}
+module.exports = { MYSQLPromiseObjectBuilder };
 
 class Profile extends Command {
 	constructor(client) {
