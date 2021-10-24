5c35eb58f06a75b294ee2a94851adebfaf263a90
IGT | WatchfulEye <82226864+CO-RONA@users.noreply.github.com>
2021-10-24 14:54:34 +0300

    Update guild-manager.js

--- dashboard/routes/guild-manager.js
+++ dashboard/routes/guild-manager.js
@@ -29,7 +29,7 @@ router.get("/:serverID", CheckAuth, async(req, res) => {
 	res.render("manager/guild", {
 		allCanSee: canSeeArray,
 		role: await req.client.getRoles(req.session.user.id),
-		latestTPS: utils.getTPS(req),
+		latestTPS: utils.getTPS(req.client),
 		playerAmount: await req.client.getPlayersLength(),
 		ownerID: config.owner.id,
 		serverID: config.serverID,
